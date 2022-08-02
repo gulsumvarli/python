22a6b9c78b62563152576ff8ddedf31f0233a144
Ümmü Gülsüm Varlı <93592493+gulsumvarli@users.noreply.github.com>
2022-08-03 00:15:48 +0300

    Add files via upload

---
+++ deger_tip.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#degerler ve tipler
+
+#değerler: program içerisinde, anlam taşıyan yapılardır.
+#programın hafızasında yer tutar.
+
+#tipler: bu hafızanın hangi formatta olacağını söyler.
+
+
+print("hello world")
+type("hello world")
+type(2)
+print(type(4.5))
+type(4.5)
+
+#Ekrana "Selam Python" ifadesinin tipini yazabilir misiniz?
+
+print(type("selam python"))
